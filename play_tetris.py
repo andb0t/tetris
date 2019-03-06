ffbceca328b9f9da110c94a523163556ff430ec1
Andreas Alexander Maier <andb0t@pm.me>
2019-03-06 21:29:21 +0100

    Allow call from superfolder

---
+++ play_tetris.py
@@ -0,0 +1,12 @@
+"""This file plays tetris."""
+from MaTris import matris
+
+
+def play():
+    print("Start!")
+    matris.start_game()
+    print("End!")
+
+
+if __name__ == "__main__":
+    play()
