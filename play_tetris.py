91e991d0dd4fb5ba23245c9da478e0dc27de9bbb
Andreas Alexander Maier <andb0t@pm.me>
2019-03-07 13:39:46 +0100

    Add cmd line args

--- play_tetris.py
+++ play_tetris.py
@@ -1,4 +1,5 @@
 """This file plays tetris."""
+import argparse
 import os
 import time
 
@@ -13,27 +14,37 @@ import autoplay as autop
 import utils
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--train", help='train the model', action='store_true')
+parser.add_argument("--new", help='overwrite old model', action='store_true')
+parser.add_argument("--verbose", help='verbose output', action='store_true')
+args = parser.parse_args()
+
+
 def play():
 
-    if os.path.exists('tetris.model'):
-        model = keras.models.load_model('tetris.model')
-        print("Loaded model from disk")
+    if args.train:
+        if not args.new and os.path.exists('tetris.model'):
+            model = keras.models.load_model('tetris.model')
+            print("Loaded model from disk")
+        else:
+            model = Sequential()
+            model.add(InputLayer(batch_input_shape=(224, 1)))
+            model.add(Dense(100, activation='sigmoid'))
+            model.add(Dense(100, activation='sigmoid'))
+            model.add(Dense(5, activation='linear'))
+            model.compile(loss='mse', optimizer='adam', metrics=['mae'])
     else:
-        model = Sequential()
-        model.add(InputLayer(batch_input_shape=(224, 1)))
-        model.add(Dense(100, activation='sigmoid'))
-        model.add(Dense(100, activation='sigmoid'))
-        model.add(Dense(5, activation='linear'))
-        model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+        model = None
 
-    autoplay = autop.Autoplay(model=model, verbose=True, graphics=False, speedup=True)
+    autoplay = autop.Autoplay(model=model, verbose=args.verbose, graphics=False, speedup=True)
 
     print("Start!")
     num_episodes = 10
     try:
         for i in range(num_episodes):
             start_time = time.time()
-            if autoplay.verbose:
+            if args.verbose:
                 matris.start_game(autoplay)
             else:
                 with utils.suppress_stdout_stderr():
@@ -45,10 +56,9 @@ def play():
     except KeyboardInterrupt:
         print("End!")
 
-    print("Save model to disk")
-    model.save('tetris.model')
-
-    print("Done!")
+    if args.train:
+        model.save('tetris.model')
+        print("Saved model to disk")
 
 
 if __name__ == "__main__":
